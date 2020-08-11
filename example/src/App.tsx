165c43ff776ef52760488b46f8b66824d97d63a0
Ammar <ammar26497@gmail.com>
2020-08-11 22:45:28 +0500

    Setup core-ui

---
+++ example/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from '@contco/core-ui'
+import '@contco/core-ui/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
