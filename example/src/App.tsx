cf6210f78228a318406daa609a16c03833c23689
Ammar <ammar26497@gmail.com>
2020-08-13 14:50:06 +0500

    Ready to be published

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,22 +1,24 @@
-import React from 'react'
-import { ExampleComponent, Box, Flex, Text, OvalButton, Input} from '@contco/core-ui'
-import { ReactComponent as Sun } from './dark-light-mode-icon.svg'
-import '@contco/core-ui/dist/index.css'
-
-
+import React from "react";
+import {
+  Box,
+  Flex,
+  Text,
+  OvalButton,
+  Input,
+} from "@contco/core-ui";
+import { ReactComponent as Sun } from "./dark-light-mode-icon.svg";
+import "@contco/core-ui/dist/index.css";
 
 const App = () => {
   return (
     <>
-      <ExampleComponent text="Create React Library Example 😄" />
       <Box m={50}>Hello</Box>
       <Flex p={30}>World</Flex>
       <Text color={"red"}>Welcome to this library</Text>
       <OvalButton Icon={Sun}></OvalButton>
       <Input placeholder="First Name"></Input>
-      
     </>
+  );
+};
 
-  )}
-
-export default App
+export default App;
