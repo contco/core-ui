2de8e27ea604392675bb2817a2f6eee6dfa8f6e7
Ammar <ammar26497@gmail.com>
2020-08-17 13:10:50 +0500

    Latest code

---
+++ example/build/precache-manifest.2c205fbedca4c1fbf6c891a8d15deaf4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fe951d2bb1ca09d678148437df48af0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "41583cf668a4bf8158d9",
+    "url": "./static/css/main.1b9bcb54.chunk.css"
+  },
+  {
+    "revision": "31ca2d1cb80a31a3447e",
+    "url": "./static/js/2.4f3febde.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.4f3febde.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41583cf668a4bf8158d9",
+    "url": "./static/js/main.17853894.chunk.js"
+  },
+  {
+    "revision": "b00023b56602ec740f82",
+    "url": "./static/js/runtime-main.9b9af3d9.js"
+  },
+  {
+    "revision": "7d595e365d82097aadffed2193fd2119",
+    "url": "./static/media/dark-light-mode-icon.7d595e36.svg"
+  }
+]);
