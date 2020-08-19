05aa330211a3fbbad980a207d45c3a1a49d410bc
Ammar <ammar26497@gmail.com>
2020-08-19 17:35:42 +0500

    Components Updated with styled-system

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -6,22 +6,20 @@ const App = () => {
     <div>
       <Flex
         height='100vh'
-        pt={5}
         justifyContent='center'
         alignItems='center'
         flexDirection='column'
       >
-        <Text fontSize='30px'>LOGIN PAGE</Text>
+        <Text mb={4} fontSize={30}>LOGIN PAGE</Text>
         <Box mb={3}>
           <Input placeholder='Username' />
         </Box>
         <Box mb={3}>
           <Input placeholder='Password' type='password' />{' '}
         </Box>
-        <LoginButton color='white'><Text mx="23px">LOGIN</Text></LoginButton>
-        <Flex justifyContent='center' alignContent='center' flex={1} mt={4}>
-          <Text>Hello there</Text>
-        </Flex>
+        <LoginButton color='white' style={{cursor: 'pointer'}}>
+          <Text mx={23}>LOGIN</Text>
+        </LoginButton>
       </Flex>
     </div>
   )
