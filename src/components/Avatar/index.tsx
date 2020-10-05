ccb413c8c2a7c37d74f244660e3ac2e7964db2f1
Bilal Khan <billalkhn98@gmail.com>
2020-10-05 22:03:52 +0500

    avatar component updated

--- src/components/Avatar/index.tsx
+++ src/components/Avatar/index.tsx
@@ -1,41 +1,29 @@
-import React, { useState, useEffect, useRef, useLayoutEffect } from "react";
-import styled from 'styled-components'
+import React, { useState, useRef, useLayoutEffect } from "react";
+import styled from "styled-components";
 import Box from '../layout/Box'
-import Flex from '../layout/Flex'
-import Text from "../Text/index";
-import randomColors from './colors';
-
-type HoverProps = {
-  width: number
+type Props = {
+  image?: string;
+  size?: string;
+  title?: string;
+  color?: string;
+  active?: boolean;
 };
 
-const HoverContainer = styled(Box) <HoverProps>`
+type AvatarProps = {
+  active?: boolean;
+};
+
+const HoverContainer = styled(Box)`
   z-index: 1;
   position: absolute;
   min-width: ;
-  width: ${(props) => props.width}px;
+  width: ${props => props.width}px;
   visibility: hidden;
   top: 100%;
   left: 50%;
-  margin-left: -${(props) => props.width / 2}px;
+  margin-left: -${props => props.width / 2}px;
 `;
-
-const Rectangle = styled(Flex)`
-  width: max-content;
-  min-width: 10px;
-  text-align: center;
-  padding: 3px 5px;
-  height: 20px;
-	border-radius: 2px;
-  background-color: #050b21;
-  font-size: 8px;
-  letter-spacing: 0.33px;
-  color: #ffffff ;
-  align-items: center;
-  justify-content: center;
-`
-
-const Triangle = styled(Box)`
+const Triangle = styled.div`
   width: 0;
   height: 0;
   border: solid 8px;
@@ -43,88 +31,58 @@ const Triangle = styled(Box)`
   margin: 0 auto;
 `;
 
+const Rectangle = styled(Box)`
+  width: max-content;
+  min-width: 10px;
+  text-align:center;
+  padding: 3px 5px;
+  height: 20px;
+  border-radius: 2px;
+  background-color: #050b21;
+  font-size: 8px;
+  letter-spacing: 0.33px;
+  color: #ffffff !important;
+  align-items: center;
+  justify-content: center;
+  display: flex;
+`;
+
 const Wrapper = styled(Box)`
   &:hover ${HoverContainer} {
     visibility: visible;
   }
   position: relative;
+  display: flex;
 `;
-
-const AvatarComponent = styled(Flex) <any>`
+const AvatarImage = styled.div<AvatarProps>`
   border-radius: 50%;
-  border: ${(props) => (props.active ? '1px solid #3498db;' : 'none;')}
+  border: ${(props) => (props.active ? "1px solid #3498db;" : "none;")};
+`;
+const MediumAvatar = styled(AvatarImage)`
+  height: 50px;
+  width: 50px;
+`;
+const SmallAvatar = styled(AvatarImage)`
   height: 20px;
   width: 20px;
-  &.lg {
-    height: 70px;
-    width: 70px;
-  }
-  &.md {
-    height: 50px;
-    width: 50px;
-  }
-`
-const NameText = styled(Text)`
-  font-size: 9px;
-  &.md {
-    font-size: 16px;
-  }
-  &.lg {
-  font-size: 25px;
-  }
-`
-
-
-type Props = {
-  image?: string
-  size?: string
-  title?: string
-  color?: string
-  active?: boolean
-  name?: string
-};
-
+  background: ${(props) => (props.color ? props.color : "none")};
+`;
+const MmAvatar = styled(AvatarImage)`
+  height: 50px;
+  width: 50px;
+  margin: 15px;
+`;
 
 const Avatar: React.FC<Props> = ({
-  image = '',
-  size = 'sm',
-  title = '',
-  color = '',
-  name,
-  active
+  image = "",
+  size = "sm",
+  title = "",
+  color = "",
+  active,
 }) => {
-  const [width, setWidth] = useState<number>(0)
-  const [randomColor, setRandomColor] = useState<string>('#555')
-  const [userName, setUserName] = useState<string>('')
+
+  const [width, setWidth] = useState(0);
   const containerRef = useRef<HTMLDivElement>();
-
-  const generateColor = (initials: string): string => {
-    const charIndex = initials.charCodeAt(0) - 65;
-    const colorIndex = charIndex % 19;
-    return randomColors[colorIndex];
-  };
-
-  const generateInitials = (name: string): string => {
-    const nameSplit = name.trim().split(' ');
-    let initials = '';
-    if (nameSplit.length === 1) {
-      initials = nameSplit[0].charAt(0).toUpperCase()
-    } else {
-      initials =
-        nameSplit[0].charAt(0).toUpperCase() +
-        nameSplit[nameSplit.length - 1].charAt(0).toUpperCase()
-    }
-    return initials;
-  }
-
-  useEffect(() => {
-    if (name) {
-      const initials = generateInitials(name);
-      const initialsColor = generateColor(initials);
-      setRandomColor(initialsColor);
-      setUserName(initials);
-    }
-  }, [name]);
 
   useLayoutEffect(() => {
     if (containerRef.current) {
@@ -134,34 +92,59 @@ const Avatar: React.FC<Props> = ({
     }
   });
 
-
-  return (
-    <Box>
-      <Wrapper>
-        {image ?
-          <AvatarComponent active={active} className={size} as='img' alt="avatar" src={image} />
-          : (
-            <AvatarComponent
-              className={size}
-              active={active}
-              bg={name ? randomColor : color}
-              alignItems='center'
-              justifyContent="center"
-            >
-              {userName !== "" ?
-                <NameText className={size} color="#fff" fontWeight="bold">{userName}</NameText>
-                : null}
-            </AvatarComponent>
+  if (size === "md") {
+    return (
+      <Wrapper >
+        {image ? (
+          <MediumAvatar active={active} as="img" src={image} />
+        ) : (
+            <MediumAvatar active={active} color={color} />
           )}
         {title ? (
+          <HoverContainer>
+            <Triangle></Triangle>
+            <Rectangle>{title}</Rectangle>
+          </HoverContainer>
+        ) : (
+            ""
+          )}
+      </Wrapper>
+    );
+  } else if (size === "mm") {
+    return (
+      <Wrapper>
+        {image ? (
+          <MmAvatar active={active} as="img" src={image} />
+        ) : (
+            <MmAvatar active={active} color={color} />
+          )}
+        {title ? (
+          <HoverContainer>
+            <Triangle></Triangle>
+            <Rectangle>{title}</Rectangle>
+          </HoverContainer>
+        ) : (
+            ""
+          )}
+      </Wrapper>
+    );
+  } else {
+    return (
+      <Wrapper>
+        {image ?
+          <SmallAvatar active={active} as="img" src={image} />
+          :
+          <SmallAvatar active={active} color={color} />
+        }
+        {title ? (
           <HoverContainer width={width} key={title}>
-            <Triangle />
+            <Triangle></Triangle>
             <Rectangle ref={containerRef}>{title}</Rectangle>
           </HoverContainer>
         ) : null}
       </Wrapper>
-    </Box>
-  )
-}
+    );
+  }
+};
 
-export default Avatar
+export default Avatar;
